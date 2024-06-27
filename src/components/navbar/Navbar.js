92af5a600f2a717992550ab12d8ff8fedc830393
S Jeevani <sabbinenijeevani@gmail.com>
2024-06-27 22:57:16 +0530

    Made changes to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -16,8 +16,8 @@ function CustomNavbar() {
             <NavDropdown.Item as={NavLink} to="/placementData">Placement Data</NavDropdown.Item>
             <NavDropdown.Item as={NavLink} to="/medainPlacemnts">More Info</NavDropdown.Item>
           </NavDropdown>
-          <Nav.Link as={NavLink} to="https://postgraduation-frontend.vercel.app/" target="_blank" rel="noopener noreferrer">Higher Studies</Nav.Link>
-          <Nav.Link as={NavLink} to="https://edcell-fp-frontend.vercel.app/" target="_blank" rel="noopener noreferrer">Ed Cell</Nav.Link>
+          <Nav.Link as={NavLink} to="https://postgraduation-frontend.vercel.app/" target="_self" rel="noopener noreferrer">Higher Studies</Nav.Link>
+          <Nav.Link as={NavLink} to="https://edcell-fp-frontend.vercel.app/" target="_self" rel="noopener noreferrer">Ed Cell</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
